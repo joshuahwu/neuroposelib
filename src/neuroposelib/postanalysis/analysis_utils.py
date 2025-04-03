2d69e926c1407f6145003f4410bc637ba0223ee8
data-hound <sabathanshuman@gmail.com>
2025-04-03 15:56:49 -0400

    migrating post clustering analysis using Shaun's fucntions to python rebased onto docs

---
+++ src/neuroposelib/postanalysis/analysis_utils.py
@@ -0,0 +1,250 @@
+### Dappy Utilities
+import dappy.DataStruct as ds
+import dappy.visualization as vis
+from dappy import read, write
+from dappy import preprocess
+from dappy import write
+from dappy import features
+from dappy import analysis
+from dappy.embed import Watershed, Embed
+
+import numpy as np
+import pandas as pd
+import pickle
+import sys
+import re
+from tqdm import tqdm
+import os
+import copy
+import time
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+import seaborn as sns
+
+from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
+from scipy.spatial.distance import squareform
+import scipy.spatial.distance as distances
+import scipy.stats as stats
+
+# from scipy.stats.contingency import expected_freq
+from scipy.stats import chisquare
+
+from skimage.segmentation import watershed
+# from skimage import measure
+
+import dask
+
+
+
+def map_to_group(
+    inpdf,
+    column_name='Condition', 
+    map_dict={
+        'Habituation': 1, 
+        'Demo':2
+        },
+    do_col_rename = None
+    ):
+    """
+        Does the following if all params are given:
+        # df.loc[:,'GroupID'] = df.Condition
+        # df.loc[df.Condition=='AssayTest','GroupID'] = 1
+        # df.loc[df.Condition=='Control','GroupID'] = 2
+        # df.loc[df.Condition=='LPS083','GroupID'] = 3
+        # df = df.rename(columns={"Condition": "ConditionName", "Timepoint": "Condition"})
+    """
+    
+    inpdf[column_name] = pd.Categorical(inpdf[column_name].astype(str))
+    inpdf['GroupID'] = inpdf[column_name].cat.codes + 1
+    
+    if do_col_rename is not None:
+        inpdf = inpdf.rename(columns=do_col_rename) 
+
+def get_freq_df(config, file_name, idx_ax_name='Clusters', col_ax_name='id'):
+    cf = pd.read_csv(''.join([config['out_path'],f'/{file_name}']), index_col=0)
+    
+    # if columns have non-string name, change them to int
+    try:
+        cf.columns = cf.columns.values.astype(int)
+    except ValueError as ve1:
+        if 'invalid literal for int() with base 10:' in str(ve1):
+            cf.columns = cf.columns.values.astype(float).astype(int)
+    
+    cf = cf.rename_axis(idx_ax_name, axis="index")
+    cf = cf.rename_axis(col_ax_name, axis="columns")
+
+    return cf
+
+def get_data_obj(
+    config,
+    file_name,
+    col_renames = None,
+    group_id_col = 'Timepoint',
+    ):
+    with open(''.join([config['out_path'], f"/{file_name}"]), "rb") as dobj:
+        data_obj = pickle.load(dobj)
+    
+    df = data_obj.data
+    mf = data_obj.meta
+    mff = data_obj.meta_by_frame
+
+    dfs_to_map = [df, mf, mff]
+    if col_renames is not None:
+        # col_rename = {"Condition": "ConditionName", "Timepoint": "Condition"}
+        col_rename = col_renames
+
+        for idx,df_ in enumerate(dfs_to_map):
+            print(idx)
+            map_to_group(df_, column_name=group_id_col, do_col_rename=col_rename)
+            df_ = df_.rename(columns=col_rename, inplace=True)
+    
+    return df, mf, mff
+
+def get_pivot_table(mf, freq_df, 
+                    relevant_columns_frm_meta = ['Condition', 'GroupID', 'AnimalID', 'id'],
+                    piv_col = 'Clusters', 
+                    piv_vals = 'occupancy',
+                    index_level = [ 'Condition',
+                                    'Clusters', 
+                                    'GroupID', 
+                                    'id', 
+                                    'AnimalID'],
+                    ):
+
+    group = mf.loc[:,relevant_columns_frm_meta]
+    occu_by_group = pd.merge(freq_df.stack().to_frame(piv_vals).reset_index(), group, on='id', 
+                                validate='many_to_one')
+    pivot_idx = [col for col in index_level if col not in [piv_col, piv_vals] ]
+    as_og = occu_by_group.set_index(index_level).sort_index(level=index_level)
+    as_og_pivot = as_og.reset_index().pivot(index=pivot_idx, columns=piv_col, values=piv_vals)
+
+    return as_og_pivot, pivot_idx
+
+def centroid_distance(arr_1, arr_2, axis=0, distance_metric=distances.euclidean):
+    # print(f"Shape of arr1 = {arr_1.shape}, Shape of arr2 = {arr_2.shape}")
+    cent_1 = arr_1.mean(axis=axis)
+    cent_2 = arr_2.mean(axis=axis)
+
+    # print(f"Shape of cent1 = {cent_1.shape}, Shape of cent2 = {cent_2.shape}")
+    # if len(cent_1.shape) > 1:
+    # return distances.cdist(cent_1, cent_2)
+    # else:
+    return distances.euclidean(cent_1, cent_2)
+
+
+@dask.delayed
+def get_shuffled_stat(occu_data, len_gp_1, len_gp_2, distance_function):
+
+        # Do a random permutation
+        rand_perm = np.random.permutation(occu_data)
+        gp_1 = rand_perm[:len_gp_1]
+        gp_2 = rand_perm[len_gp_1:]
+
+        len_gp_1 = gp_1.shape[0]
+        len_gp_2 = gp_2.shape[0]
+
+        shuffled_stat = distance_function(gp_1,gp_2)
+
+        return shuffled_stat
+
+def do_permutation_test(pivot_df, 
+                        group_names, 
+                        ndraws=None, 
+                        distance_function=centroid_distance,
+                        **kwargs2):
+    """
+    Lets say there are 2 groups, and 
+        1. We label the mouse randomly
+        2. Take the difference between the centroids of the 2 groups as test-statistic
+        3. Plot the test statistic distribution, and check the p-value for the position of actual statistic
+    
+    Inputs:
+        pivot_df: dataframe of the form generated by above script
+                | Condition | GroupID | AnimalID | 0 | 1 | 2 | 3 | 4 |
+                |---|---|---|---|---|---|---|---|
+                | baseline | 1686940_both | 0 | 0.00 | 0.00 | 0.000139 | 0.006556 | 0.000000 |
+                | baseline | 1686940_left | 0 | 0.00 | 0.00 | 0.000000 | 0.008056 | 0.000000 |
+                | habituation | 21686940_both | 0 | 0.00 | 0.000306 | 0.002250 | 0.000000 | 0.000000 |
+                | habituation | 21686940_none | 0 | 0.00 | 0.000000 | 0.000000 | 0.000000 | 0.000000 |
+        group_names: List of condition names
+        ndraws: 
+    """
+    if ndraws is None:
+        ndraws=10000
+    
+    subsample_df = pivot_df.loc[group_names].reset_index()
+    gp_1 = pivot_df.loc[group_names[0]]
+    gp_2 = pivot_df.loc[group_names[1]]
+    len_gp_1 = gp_1.shape[0]
+    len_gp_2 = gp_2.shape[0]
+
+    assert len_gp_1+len_gp_2 == subsample_df.shape[0]
+
+    act_stat = distance_function(gp_1.values,gp_2.values)
+
+    cols = subsample_df.columns
+    cluster_cols = [col for col in cols if isinstance(col,int)]
+    occupancy_data = subsample_df[cluster_cols]
+    
+    test_stat_dist = np.zeros(ndraws)
+    delayed_results = []
+    for draw in range(ndraws):
+
+        # Do a random permutation
+        # rand_perm = np.random.permutation(occupancy_data)
+        # gp_1 = rand_perm[:len_gp_1]
+        # gp_2 = rand_perm[len_gp_1:]
+
+        # len_gp_1 = gp_1.shape[0]
+        # len_gp_2 = gp_2.shape[0]
+
+        # shuffled_stat = distance_function(gp_1,gp_2)
+
+        # test_stat_dist[draw] = shuffled_stat
+
+        # Try dask based parallelization
+        shuffled_stat = get_shuffled_stat(occupancy_data, len_gp_1, len_gp_2, distance_function)
+        delayed_results.append(shuffled_stat)
+
+    test_stat_dist = np.asarray(dask.compute(*delayed_results))
+    
+    count_more_extreme = np.sum(test_stat_dist >= act_stat)
+    p_value = count_more_extreme / len(test_stat_dist)
+
+    return p_value, test_stat_dist, act_stat
+
+
+def do_pairwise_tests(pvt_tab, 
+                        ndraws=1000000, 
+                        condition='Condition', 
+                        ret_dists=False,
+                        nbins=10
+                    ):
+    uniq_conds = pvt_tab.index.get_level_values(condition).astype(str).unique()
+    num_unq_conds = len(uniq_conds)
+    heatmap = np.zeros((num_unq_conds, num_unq_conds))
+    if ret_dists:
+        stat_dists = np.zeros((num_unq_conds, num_unq_conds, 2, nbins+1))
+        act_stats = np.zeros((num_unq_conds, num_unq_conds))
+    
+
+    for idx1, cond1 in enumerate(uniq_conds):
+        for idx2, cond2 in enumerate(uniq_conds):
+            p_val, stat_dist, act_stat = do_permutation_test(
+                                                    pvt_tab, 
+                                                    [cond1, cond2], 
+                                                    ndraws=ndraws)
+            heatmap[idx1,idx2] = p_val
+            if ret_dists:
+                st_hist, st_bins = np.histogram(stat_dist)
+                stat_dists[idx1,idx2,0,1:] = st_hist
+                stat_dists[idx1,idx2,1,:] = st_bins
+                act_stats[idx1,idx2] = act_stat
+
+    if ret_dists:
+        return heatmap, stat_dists, act_stats
+    else:
+        return heatmap
+    
---
+++ src/neuroposelib/postanalysis/cross_behav_compare_utils.py
@@ -0,0 +1,194 @@
+from dappy import read
+from dappy import vis
+from dappy import preprocess
+from dappy import write
+from dappy import features
+from dappy import analysis
+from dappy import DataStruct as ds
+from dappy.embed import Embed
+from dappy.embed import Watershed
+import pandas as pd
+import numpy as np
+
+
+def get_data_obj(
+                ego_pose, labels, config, pose, ids, meta, meta_by_frame, connectivity,
+                categories = ["ego_euc"], 
+                n_pcs = 5, 
+                method="fbpca", 
+                fps = 90,
+                ):
+    from dappy import DataStruct as dsnew
+    pc_feats, pc_labels = features.pca(
+                                        ego_pose, labels, categories=["ego_euc"], n_pcs=n_pcs, method=method
+                                    )
+
+    del ego_pose, labels
+
+    wlet_feats, wlet_labels = features.wavelet(
+        pc_feats, pc_labels, ids, f_s=fps, freq=np.linspace(1, 25, 25), w0=5
+    )
+
+    # PCA on wavelet features
+    pc_wlet, pc_wlet_labels = features.pca(
+        wlet_feats,
+        wlet_labels,
+        categories=["wlet_ego_euc"],
+        # categories=["wlet_ang"],
+        n_pcs=n_pcs,
+        method=method,
+    )
+
+    del wlet_feats, wlet_labels
+    pc_feats = np.hstack((pc_feats, pc_wlet))
+    pc_labels += pc_wlet_labels
+    del pc_wlet, pc_wlet_labels
+
+    data_obj = dsnew.DataStruct(
+        pose=           pose,
+        id=             ids,
+        meta=           meta,
+        meta_by_frame=  meta_by_frame,
+        connectivity=   connectivity,
+    )
+    embedder = Embed(
+        embed_method=   config["single_embed"]["method"],
+        perplexity=     config["single_embed"]["perplexity"],
+        lr=             config["single_embed"]["lr"],
+    )
+    
+    data_obj.features = pc_feats
+    data_obj = data_obj[:: config["downsample"], :]
+    data_obj.embed_vals = embedder.embed(data_obj.features, save_self=True)
+    data_obj.ws = Watershed(
+        sigma=config["single_embed"]["sigma"], max_clip=1, log_out=True, pad_factor=0.05
+    )
+    data_obj.data["Cluster"] = data_obj.ws.fit_predict(data=data_obj.embed_vals)
+
+    return data_obj
+
+
+def populate_new_data_obj(data_obj, config, 
+                            pose = None, 
+                            ids = None, 
+                            meta = None, 
+                            meta_by_frame = None, 
+                            connectivity = None
+                        ):
+    '''
+    Populate a new data object by performing clustering and embedding again on the features extracted from
+    the provided data object
+    '''
+    from dappy import DataStruct as dsnew
+
+    if pose is None and ids is None and meta is None and meta_by_frame is None and connectivity is None:
+        new_data_obj = copy.deepcopy(data_obj)
+    else:
+        new_data_obj = dsnew.DataStruct(
+            pose=           pose,
+            id=             ids,
+            meta=           meta,
+            meta_by_frame=  meta_by_frame,
+            connectivity=   connectivity,
+        )
+        new_data_obj = new_data_obj[:: config["downsample"], :]
+        new_data_obj.features = data_obj.features
+
+    embedder = Embed(
+        embed_method=   config["single_embed"]["method"],
+        perplexity=     config["single_embed"]["perplexity"],
+        lr=             config["single_embed"]["lr"],
+    )
+
+    '''
+        # This embedding seems to be the key step. After this the cluster numbers and assignments do not change
+        # evs_2 = body_embedder.embed(data_obj_1.features, save_self=True)
+        # ws_2 = copy.deepcopy(data_obj_1.ws)
+        # clusters_2 = ws_2.fit_predict(data=evs_2)
+        # clusters_3 = ws_2.fit_predict(data=evs_2)
+        # np.isclose(clusters_2, clusters_3).all() -> Gives True
+
+    '''
+    
+    new_data_obj.embed_vals = embedder.embed(data_obj.features, save_self=True)
+    new_data_obj.ws = Watershed(
+        sigma=config["single_embed"]["sigma"], max_clip=1, log_out=True, pad_factor=0.05
+    )
+    new_data_obj.data["Cluster"] = new_data_obj.ws.fit_predict(data=new_data_obj.embed_vals)
+
+    return new_data_obj
+
+
+def get_clust_frame_groupings(data_obj_1):
+    df_1 = data_obj_1.data
+    df_1_clust_frame = df_1.groupby('Cluster')['frame'].apply(np.array).reset_index().set_index('Cluster')
+
+    return df_1_clust_frame
+
+def best_matches(df1, df2):
+    # match_results = pd.DataFrame(index=df1.index, columns=df1.columns)
+    match_results = df1.copy(deep=True)
+    
+    for df1_index, row in df1.iterrows():
+        for col, arr1 in row.items():
+            best_match = None
+            best_intersection = 0
+            best_intersection_len = 0
+            if isinstance(arr1, str):
+                arr1 = np.fromstring(arr1.strip('[]'), sep=' ', dtype='int')
+            
+            for df2_index, row2 in df2.iterrows():
+                arr2 = row2[col]
+
+                if isinstance(arr2, str):
+                    arr2 = np.fromstring(arr2.strip('[]'), sep=' ', dtype='int')
+                
+                intersection_size = np.intersect1d(arr1, arr2).size
+                print(intersection_size, arr1.dtype, arr2.dtype)
+                
+                if intersection_size > best_intersection:
+                    best_intersection = intersection_size
+                    best_match = df2_index
+                    best_intersection_len = arr2.size
+                    # print ('Best Intersection Size = ', best_intersection_len)
+            
+            match_results.loc[df1_index, "ClusterMatch"] = best_match
+            if best_match:
+                match_results.loc[df1_index, "MatchFrom%"] = (best_intersection/arr1.size)*100
+                match_results.loc[df1_index, "MatchTo%"] = (best_intersection/best_intersection_len)*100
+            else:
+                match_results.loc[df1_index, "MatchFrom%"] = 100.0
+                match_results.loc[df1_index, "MatchTo%"] = 0.0
+            
+            
+    
+    return match_results
+
+
+def get_clust_frame(ego_pose=None, labels=None, config=None, pose=None, ids=None, meta=None, meta_by_frame=None, connectivity=None,
+                    data_obj=None,
+                    categories = ["ego_euc"], 
+                    n_pcs = 5, 
+                    method="fbpca", 
+                    fps = 90,
+                    ):
+    
+    if ego_pose is None         and \
+        data_obj is not None    :
+
+        dobj = populate_new_data_obj(data_obj, config, pose, ids, meta, meta_by_frame, connectivity)
+    else:
+        dobj = get_data_obj(  ego_pose, 
+                            labels, 
+                            config, 
+                            pose, 
+                            ids, 
+                            meta, 
+                            meta_by_frame, 
+                            connectivity,
+                        )
+    
+    return get_clust_frame_groupings(dobj), dobj.data
+
+
+
