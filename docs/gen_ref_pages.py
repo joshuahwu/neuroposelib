664b1327de1f00ec0af31e1ebbffad6a5de6c4c0
Joshua Wu <wu.joshuah@gmail.com>
2025-12-05 15:50:35 -0500

    neuroposelib/index.md

--- docs/gen_ref_pages.py
+++ docs/gen_ref_pages.py
@@ -45,5 +45,5 @@ for path in sorted(Path("src").rglob("*.py")):
 
     mkdocs_gen_files.set_edit_path(full_doc_path, path)
 
-with mkdocs_gen_files.open("reference/neuroposelib.md", "w") as nav_file:
+with mkdocs_gen_files.open("reference/neuroposelib/index.md", "w") as nav_file:
     nav_file.writelines(nav.build_literate_nav())
